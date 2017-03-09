91f7160aae96dd23786901dec3a9aae686e88af4
Unknown <gitclase@gmail.com>
2017-03-09 19:01:16 +0100

    Ejercicio 7

---
+++ 07_alarma/alarma/src/com/miriamag/SensorMovimiento.java
@@ -0,0 +1,69 @@
+package com.miriamag;
+
+import java.util.Random;
+
+/**
+ * Created by 7FPROG11 on 09/03/2017.
+ */
+public class SensorMovimiento {
+
+
+    // Propiedades
+    private int umbral;
+
+    private Random r = new Random();
+
+    // Métodos del sensor
+    public boolean hayMovimiento() {
+        return r.nextInt(100) + 1 > umbral;
+    }
+
+    // Asociación
+    private Alarma alarma;
+
+    // Código generado
+
+    public SensorMovimiento(int umbral) {
+        this.umbral = umbral;
+    }
+
+    public SensorMovimiento() {
+        umbral = r.nextInt(100) + 1;
+    }
+
+    public int getUmbral() {
+        return umbral;
+    }
+
+    public void setUmbral(int umbral) {
+        this.umbral = umbral;
+    }
+
+    public Alarma getAlarma() {
+        return alarma;
+    }
+
+    public void setAlarma(Alarma alarma) {
+        this.alarma = alarma;
+    }
+
+    @Override
+    public String toString() {
+        return "SensorMovimiento{" +
+                "umbral=" + umbral +
+                '}';
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
